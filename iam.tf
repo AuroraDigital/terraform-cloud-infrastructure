1608ae437ffc0eff27467ba15642b05f6ebbe554
pfcodes <phil@auroradigit.al>
2020-12-18 03:24:38 -0800

    Create iam.tf

---
+++ iam.tf
@@ -0,0 +1,23 @@
+resource "aws_iam_user" "user" {
+  name = "user"
+  tags = {
+    Terraform = true
+  }
+}
+
+resource "aws_iam_user_policy" "policy" {
+  name   = "user-policy"
+  user   = aws_iam_user.user.name
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "s3:*",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
