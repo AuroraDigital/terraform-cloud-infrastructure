e50950ec2275a11bdbc38839d47b681caf959c11
pfcodes <phil@auroradigit.al>
2021-01-02 12:48:12 -0800

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -10,7 +10,7 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
-  enable_nat_gateway = true
+  enable_nat_gateway = true # warning: NATs come with additional costs
 
   tags = {
     Terraform   = "true"
